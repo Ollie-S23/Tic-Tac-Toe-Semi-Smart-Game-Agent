2d6993cd264832e1da8b37e1a6fe89636b78bdc1
Oliver Scott <105912692@student.swin.edu.au>
2026-03-10 11:26:44 +1100

    dumb agent choice basic implementation

--- agent.py
+++ agent.py
@@ -5,12 +5,17 @@ class enemy_agent:
     def __init__(self):
         pass
 
-    def check_board_state(self):
+    def check_board_state(self, freeSpots):
         smartChance = random.randint(0,1) #binary options: 0,1
         #dumb result - put random place
         if (smartChance == 0):
             print("check_board_state was 0. No smart")
-            choice = random.randint(0,8) #0 - 8 (9 options)
+            while(True):
+                choice = random.randint(0,8) #0 - 8 (9 options)
+                if (choice in freeSpots):
+                    freeSpots.remove(choice)
+                    print(freeSpots)
+                    break
             print(choice)
         #smart result - check board condition
         else:
@@ -25,10 +30,12 @@ class enemy_agent:
 def main():
     
     game_status = "play"
+    freeSpots = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
     while (game_status == "play"):
         enemy = enemy_agent()
-        enemy.check_board_state()
+        freeSpots = [0, 3, 5, 7]
+        enemy.check_board_state(freeSpots)
         break
 
 if __name__ == "__main__":
